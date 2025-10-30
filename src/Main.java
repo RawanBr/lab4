b33f64ed09284da22eeea63859849b365873a753
dev <dev@test.com>
2025-10-30 13:54:02 +0300

    inital commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,11 +7,13 @@ public class Main {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
+        ArrayList<Shape> tests = new ArrayList<>();
+
 
     boolean valid = true;
 
     while (valid) {
-        System.out.println("What shape do you want to test?\n1- Circle\n2- Rectangle\n3- Triangle\n(ENTER 0 TO EXIT)");
+        System.out.println("What shape do you want to test?\n1- Circle\n2- Rectangle\n3- Triangle\n4- View the tests history\n(ENTER 0 TO EXIT)");
         int choice = input.nextInt();
 
         switch (choice) {
@@ -18,6 +21,7 @@ public class Main {
                 System.out.println("Enter the radius of the circle : ");
                 double radius = input.nextDouble();
                 Circle circle = new Circle(radius);
+                tests.add(circle);
 
                 System.out.println("------------------------------");
                 System.out.println(circle);
@@ -30,6 +34,7 @@ public class Main {
                 System.out.println("Enter the width of the rectangle: ");
                 double width = input.nextDouble();
                 Rectangle rectangle = new Rectangle(heigh, width);
+                tests.add(rectangle);
 
                 System.out.println("------------------------------");
                 System.out.println(rectangle);
@@ -42,9 +47,20 @@ public class Main {
                 System.out.println("Enter the base of the triangle: ");
                 double base = input.nextDouble();
                 Triangle triangle = new Triangle(height, base);
+                tests.add(triangle);
 
                 System.out.println("------------------------------");
                 System.out.println(triangle);
+                System.out.println("------------------------------");
+                break;
+
+            case 4:
+                System.out.println("------------------------------");
+
+                System.out.println("Here are all the shapes you tested before: \n");
+                for (Shape test : tests) {
+                    System.out.println(test + "\n");
+                }
                 System.out.println("------------------------------");
                 break;
 
